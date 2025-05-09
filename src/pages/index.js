3df65949c129793529ec2a161708d2e7eadbfd44
aperalta03 <alonso.pe03@gmail.com>
2025-05-08 21:57:56 -0500

    New Description

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,11 @@ export default function Home() {
         <Card
             name = "Alonso Peralta"
             position = "Software Engineer"
-            aboutMe = {`Hi there! I'm Alonso Peralta, a seasoned web developer passionate about crafting clean and dynamic websites. With 5 years of experience, I specialize in front-end and back-end development using HTML, CSS, JavaScript, and frameworks like React and Angular.
-
-            I thrive on creating user-centric designs and optimizing performance for seamless browsing experiences. Let's collaborate TOGETHER!`}
-
+            aboutMe =  {
+                `Hi there! I'm Alonso Peralta, an aspiring software engineer passionate about crafting efficient and user-friendly tools for business and clients. 
+                With ~2 years of experience, I specialize in Full Stack Development and AI/ML using Python, JavaScript, and frameworks like React.
+                I thrive on creating client-centric enviroments and optimizing performance for speedy and secure experiences. Contact me for opportunities!`
+            }
             linkedin="https://www.linkedin.com/in/aperalta03"
             github="https://github.com/aperalta03"
             cvLink="/files/resume.pdf"
