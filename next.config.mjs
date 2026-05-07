f8a9ab25340bb29684b76e992e682f0d1314bd68
aperalta03 <aperalta03@tamu.edu>
2026-05-07 17:43:21 -0500

    Updated to just simple card + new code repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: false,
-    },
-  };
-  
+  output: 'export',
+  images: { unoptimized: true },
+};
+
 export default nextConfig;
